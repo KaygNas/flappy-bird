2128bfef3bbe00b06d3cbabf97da4427ed3e27ab
huangchengjie <huangchengjie@dm-ai.com>
2021-12-19 23:32:50 +0800

    发布 github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [],
+	build: { outDir: resolve(__dirname, './docs') },
+	base: './',
 })
